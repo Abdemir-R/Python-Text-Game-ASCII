c8222f965a97477f8d8d5de68039938ef783a8c3
Abdemir-R <163116937+Abdemir-R@users.noreply.github.com>
2024-03-21 01:57:31 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,18 @@
+from character import Hero, Enemy
+from weapon import short_bow, iron_sword
 
+hero = Hero(name="Hero", health=100, damage=5)
+hero.equip(iron_sword)
+enemy = Enemy(name="Enemy", health=100,damage=3, weapon=short_bow)
+
+while True:
+    hero.attack(enemy)
+    enemy.attack(hero)
+
+    hero.health_bar.draw()
+    enemy.health_bar.draw()
+
+    print(f"Health of {hero.name} {hero.health}")
+    print(f"Damage of {enemy.name} {enemy.health}")
+
+    input()
